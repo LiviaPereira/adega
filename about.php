8ed27754944cb9122b324672f0ac21656fc8f4a8
Murillo Maciel de Arruda <murillo.arruda@gmail.com>
2020-04-22 16:46:20 -0300

    Atualizando arquivos

--- about.php
+++ about.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+   <!-- montserrat-regular-400 -->
+   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"> 
+    <!-- amita-regular-400 -->
+    <link href="https://fonts.googleapis.com/css2?family=Amita&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/about.css">
     <title>Sobre</title>
--- index.php
+++ index.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- montserrat-regular-400 -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+   <!-- montserrat-regular-400 -->
+   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"> 
+    <!-- amita-regular-400 -->
+    <link href="https://fonts.googleapis.com/css2?family=Amita&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/home.css">
     <title>Adega a Jato</title>
