82557ec67a45ca275519bf3bc31db9fd9feb0105
Murillo Maciel de Arruda <murillo.arruda@gmail.com>
2020-04-23 10:20:34 -0300

    atualizando semantica html do header.php

--- includes/header.php
+++ includes/header.php
@@ -1,93 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-   <!-- montserrat-regular-400 -->
-   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"> 
-    <!-- amita-regular-400 -->
-    <link href="https://fonts.googleapis.com/css2?family=Amita&display=swap" rel="stylesheet">
-    
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/header.css">
-    <title>header</title>
-</head>
-<body>
-    
+    <header>
+        <div class="header-fixed">
 
-<header>
-
-<div class="header-fixed">
-
-    <div class="top"><p>Adega a Jato! Pra quem tem pressa de beber!</p></div>
-
-        <div class="header-conteiner">
-            <div class="logo-site">
-                <img class="logo-img" src="./img/logo.png" alt="">
-                <spam class="amita-regular-400">VINARIAM</spam>
-            </div>
-            <div>
-                <input class="header-search-input" type="text" name="search" placeholder="Digite aqui"><button class="header-search-button" type="submit">BUSCAR</button>
-            </div>
-            <div>
-                <p>Crie Sua conta</p>
-            </div>
-            <div>
-                <p>Entre</p>
-            </div>
-            <div>
-                <img class="header-img-cart" src="./img/cart.PNG" alt="">
+            <div class="top">
+                <p>Adega a Jato! Pra quem tem pressa de beber!</p>
             </div>
 
-        </div>
-        
-        <div class="nav-conteiner">
-            <nav>
-                    <div>
-                        <a href="">
-                            <img class="header-categories-img" src="./img/wine.png" alt="">
-                            <p class="header-categories">VINHOS</p>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <img class="header-categories-img" src="./img/beer.png" alt="">
-                            <p class="header-categories">CERVEJAS</p>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <img class="header-categories-img" src="./img/whiskey.png" alt="">
-                            <p class="header-categories">DESTILADOS</p>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <img class="header-categories-img" src="./img/refri.png" alt="">
-                            <p class="header-categories">NÃO ALCOÓLICOS</p>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <img class="header-categories-img" src="./img/kits.png" alt="">
-                            <p class="header-categories">KIT'S</p>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <img class="header-categories-img" src="./img/sales.png" alt="">
-                            <p class="header-categories">PROMOÇÕES</p>
-                        </a>
-                    </div>
-                
-
-            </nav>
-
-
-        </div>
+            <div class="header-conteiner">
+                <div class="logo-site">
+                    <img class="logo-img" src="./img/logo.png" alt="">
+                    <spam class="amita-regular-400">VINARIAM</spam>
+                </div>
+                <div>
+                    <input class="header-search-input" type="text" name="search" placeholder="Digite aqui"><button class="header-search-button" type="submit">BUSCAR</button>
+                </div>
+                <div>
+                    <p>Crie Sua conta</p>
+                </div>
+                <div>
+                    <p>Entre</p>
+                </div>
+                <div>
+                    <img class="header-img-cart" src="./img/cart.PNG" alt="">
+                </div>
+            </div>
             
-</div>
-
-</header>
-</body>
-</html>
+            <div class="nav-conteiner">
+                <nav>
+                        <div>
+                            <a href="">
+                                <img class="header-categories-img" src="./img/wine.png" alt="">
+                                <p class="header-categories">VINHOS</p>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <img class="header-categories-img" src="./img/beer.png" alt="">
+                                <p class="header-categories">CERVEJAS</p>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <img class="header-categories-img" src="./img/whiskey.png" alt="">
+                                <p class="header-categories">DESTILADOS</p>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <img class="header-categories-img" src="./img/refri.png" alt="">
+                                <p class="header-categories">NÃO ALCOÓLICOS</p>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <img class="header-categories-img" src="./img/kits.png" alt="">
+                                <p class="header-categories">KIT'S</p>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <img class="header-categories-img" src="./img/sales.png" alt="">
+                                <p class="header-categories">PROMOÇÕES</p>
+                            </a>
+                        </div>
+                </nav>
+            </div>
+        </div>
+    </header>
