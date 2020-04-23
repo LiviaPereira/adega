0cb56b3641fe8e2b1e27d33a1cd73bc08cb23488
Murillo Maciel de Arruda <murillo.arruda@gmail.com>
2020-04-23 11:37:05 -0300

    atualizando footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,13 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/footer.css">
     <title>Document</title>
 </head>
 <body>
+
+
 <footer>
-    A VENDA E O CONSUMO DE BEBIDAS ALCOÓLICAS SÃO PROIBIDOS PARA MENORES DE 18 ANOS. BEBIDA ALCOÓLICA PODE CAUSAR DEPENDÊNCIA QUÍMICA E, EM EXCESSO, PROVOCA GRAVES MALES À SAÚDE. BEBA COM MODERAÇÃO. Preços, ofertas e condições exclusivas para internet e válidos
-    durante o dia de hoje, podendo sofrer alterações sem prévia notificação. Venda sujeita à disponibilidade de estoque no dia da entrega. No caso de faltar algum produto, este não será entregue e o valor correspondente não será cobrado. Vinariam S A
-    / CNPJ: 00000000/0000- 00 Av. Interlagos , 00000, CEP: 000000-000 - São Paulo - SP
+
+    <div class="footer-newsletter">
+        <h1>Cadastre-se para receber novidades</h1>
+            <div>
+                <input class="footer-input-newsletter" type="email" name="email-news" placeholder="Digite seu e-mail">
+                <button class="footer-submit-newsletter" type="submit">CADASTRAR</button>
+            </div>
+    </div>
+
+    <div class="footer-content">
+
+
+
+    </div>
+
+
+    
+
 </footer>
+
+
+
+
+
+    
 </body>
 </html>
