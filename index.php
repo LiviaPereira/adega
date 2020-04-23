ba47d0c2e94d4315ba423eb248ba6a2b5b603216
Murillo Maciel de Arruda <murillo.arruda@gmail.com>
2020-04-23 11:36:06 -0300

    adicionando o include footer ao index.php

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- montserrat-regular-400 -->
-   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"> 
-    <!-- amita-regular-400 -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css2?family=Amita&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
     <title>Adega a Jato</title>
 </head>
 
@@ -46,8 +45,8 @@
         </div>
 </main>
 
-
-
 <?php include_once('includes/footer.php'); ?>
+
+
 </body>
 </html>
