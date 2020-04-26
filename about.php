a97e7ee061f2574fa2929d45f6be68c1714a5f31
Moacibernardo <mjbernardoe@yahoo.com.br>
2020-04-26 14:30:13 -0300

    including banner in the about.php and correction in about.css

--- about.php
+++ about.php
@@ -8,34 +8,50 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <!-- amita-regular-400 -->
     <link href="https://fonts.googleapis.com/css2?family=Amita&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/header.css">
-	<link rel="stylesheet" href="css/about.css">
-    
 
     <title>Sobre</title>
 </head>
 
 <body>
-    <?php include_once('includes/header.php'); ?>
 
-    <main>
-        <section class="banner-home">
-            <p>Banner rotativo</p>
+    <div id="interface">
+        <?php include_once('includes/header.php'); ?>
 
+        <div class="banner"></div>
+
+        <section class="service spacing">
+            <div></div>
+            <div>
+                <h1>Entrega em até 24 horas</h1>
+            </div>
+            <div></div>
+            <div>
+                <h1>Frete Grátis</h1>
+            </div>
+            <div></div>
+            <div class="basis">
+                <h1>Maior Segurança na sua compra</h1>
+            </div>
         </section>
 
-    </main>
-    <div class="about-somos"> Quem Somos? </div>
-    <div class="about-description"> O Adega à jato é o ponto de encontro de quem curte uma boa bebida em qualquer situação: com os amigos, o amor da sua vida ou até mesmo a sós.
-        A loja de bebidas com a entrega mais rápida conta com os melhores vinhos, cervejas especiais e várias opções de bebidas e destilados.
-        Tudo isso agora te encontra em casa, com a entrega de qualidade e atendimento no padrão que você merece.
-        Atuamos diretamente na paixão por algo que ultrapassa o simples fato de degustar um bom vinho, trabalhamos para que tudo o que há de melhor dentro de uma garrafa chegue à sua taça em perfeita sintonia com a temperaturae a evolução da bebida só assim atingimos nossos objetivos de entrega.
-        Que tal receber uma bebida geladinha em sua residência?<a href="clique-aqui">Clique aqui para entrar em contato conosco!</a>
-    </div>
+        <main>
 
 
-    <?php include_once('includes/footer.php'); ?>
+        </main>
+        <div class="about-somos"> Quem Somos? </div>
+        <div class="about-description"> O Adega à jato é o ponto de encontro de quem curte uma boa bebida em qualquer situação: com os amigos, o amor da sua vida ou até mesmo a sós.
+            A loja de bebidas com a entrega mais rápida conta com os melhores vinhos, cervejas especiais e várias opções de bebidas e destilados.
+            Tudo isso agora te encontra em casa, com a entrega de qualidade e atendimento no padrão que você merece.
+            Atuamos diretamente na paixão por algo que ultrapassa o simples fato de degustar um bom vinho, trabalhamos para que tudo o que há de melhor dentro de uma garrafa chegue à sua taça em perfeita sintonia com a temperaturae a evolução da bebida só assim atingimos nossos objetivos de entrega.
+            Que tal receber uma bebida geladinha em sua residência?<a href="clique-aqui">Clique aqui para entrar em contato conosco!</a>
+        </div>
+
+
+        <?php include_once('includes/footer.php'); ?>
 </body>
 
 
