84263aa46877ea86544da890a028df4ea2451167
Moacibernardo <mjbernardoe@yahoo.com.br>
2020-04-29 22:28:50 -0300

    Ajustes na visualização da página quem somos - About

--- about.php
+++ about.php
@@ -37,7 +37,6 @@
                 <h1>Maior Segurança na sua compra</h1>
             </div>
         </section>
-
         <main>
 
 
@@ -46,8 +45,9 @@
         <div class="about-description"> O Adega à jato é o ponto de encontro de quem curte uma boa bebida em qualquer situação: com os amigos, o amor da sua vida ou até mesmo a sós.
             A loja de bebidas com a entrega mais rápida conta com os melhores vinhos, cervejas especiais e várias opções de bebidas e destilados.
             Tudo isso agora te encontra em casa, com a entrega de qualidade e atendimento no padrão que você merece.
-            Atuamos diretamente na paixão por algo que ultrapassa o simples fato de degustar um bom vinho, trabalhamos para que tudo o que há de melhor dentro de uma garrafa chegue à sua taça em perfeita sintonia com a temperaturae a evolução da bebida só assim atingimos nossos objetivos de entrega.
-            Que tal receber uma bebida geladinha em sua residência?<a href="clique-aqui">Clique aqui para entrar em contato conosco!</a>
+            Atuamos diretamente na paixão por algo que ultrapassa o simples fato de degustar um bom vinho, trabalhamos para que tudo o que há de melhor dentro de uma garrafa chegue à sua taça em perfeita sintonia com a temperatura e a evolução da bebida só assim atingimos nossos objetivos de entrega.
+            Que tal receber uma bebida geladinha em sua residência?
+          
         </div>
 
 
