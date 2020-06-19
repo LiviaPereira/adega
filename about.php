b6e6408298eb8388db906a2e3d57c26f463845b0
Moacibernardo <mjbernardoe@yahoo.com.br>
2020-06-19 14:24:10 -0300

    Decrease text and reduce space in relation to the footer

--- about.php
+++ about.php
@@ -19,16 +19,22 @@
 <body>
 
     <div id="interface">
-             <main>
+        <main>
 
 
         </main>
         <div class="about-somos"> Quem Somos? </div>
-        <div class="about-description"> O Adega à jato é o ponto de encontro de quem curte uma boa bebida em qualquer situação: com os amigos, o amor da sua vida ou até mesmo a sós.<br>A loja de bebidas com a entrega mais rápida conta com os melhores vinhos, cervejas especiais e várias opções de bebidas e destilados.<br>Tudo isso agora te encontra em casa, com a entrega de qualidade e atendimento no padrão que você merece.<br>Atuamos diretamente na paixão por algo que ultrapassa o simples fato de degustar um bom vinho, trabalhamos para que tudo o que há de melhor dentro de uma garrafa chegue à sua taça em perfeita sintonia com a temperatura e a evolução da bebida, somente assim atingimos nossos objetivos de entrega.<br>Que tal receber uma bebida geladinha em sua residência?
-          
+        <br>
+        <div class="about-description"> O Adega à jato é o ponto de encontro de quem curte uma boa bebida em qualquer situação: com os amigos, o amor da sua vida ou até mesmo a sós.<br>A loja de bebidas com a entrega mais rápida conta com os melhores vinhos, cervejas especiais e várias opções de bebidas e destilados.<br>Tudo isso agora te encontra em casa, com a entrega de qualidade e atendimento no padrão que você merece.<br>Atuamos diretamente na paixão por algo que ultrapassa o simples fato de degustar um boa bebida, trabalhamos para que tudo o que há de melhor dentro de uma garrafa chegue à sua taça, copo em perfeita sintonia com a temperatura e a evolução da bebida, somente assim atingimos nossos objetivos.<br>Que tal receber uma bebida geladinha e muito rápida em sua residência?
+            <br>
+            <br>
+            <br>
+               
+
         </div>
 
 
         <?php include_once('includes/footer.php'); ?>
 </body>
+
 </html>
