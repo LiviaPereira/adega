07e567347a46d83fa9c78324888bc2c99b6becf4
Murillo Maciel de Arruda <murillo.arruda@gmail.com>
2020-07-23 17:19:59 -0300

    refactoring shoppingCart page

--- resources/views/shoppingCart.blade.php
+++ resources/views/shoppingCart.blade.php
@@ -1,72 +1,80 @@
 @extends('templates.institucional')
 @section('content')
 
-
-<link rel="stylesheet" href="css/shoppingCart.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<body class="body-cart">
-
-{{--  @dd($produtos)  --}}
-
-@if ($produtos != null)
-       
-    <div class="cart-container" >
-        <div class= "cart-son">
-            <ul class="cart-list cart-descricao">
-                <li class="product-cart">Produto</li>
-                <li class="other-li">Preço</li>
-                <li class="other-li">Quantidade</li>
-                <li class="other-li">Remover</li>
-            </ul>
-
-            @foreach ($produtos as $item) 
-            {{--  @dd($produtos)  --}}
-                <section class="product-checkout">
-                    <ul class="cart-list">
-                        <li class="product-in-cart">
-                            <img src="{{$item['photo']}}" alt="Imagem do Produto">
-                            <p>{{ $item['name'] }}</p>
-                        </li>
-                        <li class="other-li">{{number_format($item['sale_price'], 2, ',', '.')}}</li>
-                        {{--  <li class="other-li quantidade"><a class="cart-links" href="#">-</a><p>1</p><a class="cart-links" href="#">+</a></li>  --}}
-                        <li class="other-li total-in-cart">{{$item['qty']}}</li>
-                        
-                        <li class="other-li quantidade"><a class="cart-links" href="/remover/{{$item['product_id']}}"><i style="font-size:24px" class="fa">&#xf014;</i></a></li>
-                    </ul>
-                </section>
-            @endforeach
-
-            {{--  @php
-                $precoTotal = $precoTotal + ($produto['qty'] * $produto['preco_venda']);
-            @endphp  --}}
-
-
-            {{--  <button type="submit"><a href="/carrinho/adicionar/{{$busca->idProduto}}">Adicionar ao Carinho</a></button>  --}}
-
-
-
-        </div>
-        {{--  <a class="cart-links" href="#"><p class="cupom">Adicionar cupom de desconto</p></a>  --}}
-        <div class="checkout">
-            <ul>
-                {{--  <li><p>Subtotal</p><p>R$ 155,00</p></li>  --}}
-                <li class="delivery"><p><img src="img/truck.png" alt="Imagem Frete Grátis"> Entrega <strong> GRÁTIS</strong></p></li>
-                <li><p>Total</p><p>R$ 155,00</p></li>
-                <li><a class="cart-links fechar-pedido" href="/finalizarCompra">FECHAR PEDIDO</a></li>
-            </ul>
-        </div>
-        <a class="cart-links" href="/"><p class="cupom">Continuar Comprando</p></a>
-    </div>
-
-@else
-
-    <div class="empty-car">
-        <h1>Carrinho</h1>
-        <p class="info">Seu carrinho está vazio.</p>
-        <a href="/"><img src="{{url('img/empty-cart.jpg')}}" alt="Imagem carrinho vazio"></a>
-    </div>
+    <link rel="stylesheet" href="{{ url('css/cart.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
-@endif
-    
+
+    @if ($produtos != null)
+
+    @php
+    $valorTotal = 0;
+    foreach($produtos as $produto){
+        $valorTotal += $produto["sale_price"] * $produto["qty"];
+    }
+    @endphp
+
+        <section id="panel">
+
+            <div class="main-container">             
+
+                <table class="table-mobile">
+                    <thead>
+                        <tr>
+                            <td>PRODUTO</td>
+                            <td>PREÇO</td>
+                            <td>QUANTIDADE</td>
+                            <td>SUBTOTAL</td>
+                            <td>REMOVER</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($produtos as $item) 
+                            <tr>
+                                <td> 
+                                    <img class="foto" src="{{$item['photo']}}" alt="Imagem do Produto">
+                                    <p class="product-name">{{ $item['name'] }}</p>
+                                </td>
+                                <td> <span>Preço<br></span> R$ {{number_format($item['sale_price'], 2, ',', '.')}} </td>
+                                <td> <span>Quantidade<br></span> {{$item['qty']}} </td>
+                                <td> <span>Subtotal<br></span> R$ {{number_format($item['qty'] * $item['sale_price'], 2, ',', '.') }} </td>
+                                <td> <span>Remover<br></span><a class="cart-links" href="/remover/{{$item['product_id']}}"><i style="font-size:24px" class="fa">&#xf014;</i></a> </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="inferior">
+            
+                <div class="checkout">
+
+                    <table class="fechar">
+                        <tr>
+                            <td><strong>Frete:</strong></td>
+                            <td>GRÁTIS</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Total:</strong></td>
+                            <td>R$ {{number_format($valorTotal, 2, ',', '.')}}</td>
+                        </tr>
+                    </table>
+                    <div id="comprarb">
+                        <button><a href="/finalizarCompra">FECHAR PEDIDO</a></button>
+                    </div>
+                </div>
+        </div>
+
+        </section>
+
+        @else
+
+            <div class="empty-car">
+                <h1>Carrinho</h1>
+                <p class="info">Seu carrinho está vazio.</p>
+                <a href="/"><img src="{{url('img/empty-cart.jpg')}}" alt="Imagem carrinho vazio"></a>
+            </div>
+        
+        @endif
+
 @endsection
