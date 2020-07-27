88741a4da904cd372a378109d975570cb971503c
Murillo Maciel de Arruda <murillo.arruda@gmail.com>
2020-07-27 11:47:20 -0300

    final
    Co-Authored-By: liviapereira <62553365+liviapereira@users.noreply.github.com>
    Co-Authored-By: igorvalerio <igorvalerio@users.noreply.github.com>
    Co-Authored-By: moacibernardo <39143947+moacibernardo@users.noreply.github.com>

--- resources/views/pages/newsletter.blade.php
+++ resources/views/pages/newsletter.blade.php
@@ -1,6 +1,7 @@
 @extends('templates.institucional')
 @section('content')
         <link rel="stylesheet" href="{{url('css/about.css')}}">
+        <link rel="stylesheet" href="{{url('css/cart.css')}}">
         <div class="about">
 
         
@@ -8,15 +9,16 @@
         @if ($exists == false)
 
             <h1>Obrigado!</h1>
-            <p>O email <strong> {{ $email }} </strong> foi inscrito com sucesso!</p>
+            <p id="info2">O email <strong> {{ $email }} </strong> foi inscrito com sucesso!</p>
             <br>
             <p>Você receberá todas nossas novidades. Além de ofertas, descontos e promoções especiais exclusivas para você! </p>
             <br>
             
         @else
 
-            <h1>Erro!</h1>
-            <p>O email <strong> {{ $email }} </strong> já está cadastrado!</p>
+            <h1>Olá!</h1>
+            <p id="info2">O email <strong> {{ $email }} </strong> já está cadastrado!</p>
+            <br>
             <p>Caso queira sair da lista de novidades <a href="/newsletter/{{$id}}">clique aqui</a>.</p>
             <br>
 
--- resources/views/pages/outnewsletter.blade.php
+++ resources/views/pages/outnewsletter.blade.php
@@ -1,11 +1,12 @@
 @extends('templates.institucional')
 @section('content')
         <link rel="stylesheet" href="{{url('css/about.css')}}">
+        <link rel="stylesheet" href="{{url('css/cart.css')}}">
         <div class="about">
 
 
             <h1>Obrigado!</h1>
-            <p>O email <strong> {{ $email->email }} </strong> foi removido com suesso!</p>
+            <p id="info">O email <strong> {{ $email->email }} </strong> foi removido com suesso!</p>
             <br>
             <p>Obrigado por participar da nossa lista de novidades! </p>
             <br>
